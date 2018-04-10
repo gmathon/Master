6f473191c034845b552a69906d59e3d4b497b912
Mathons <gmathon00@live.com>
2018-04-09 21:05:13 -0400

    final answer for problem

--- src/reversestring.java
+++ src/reversestring.java
@@ -1,7 +1,12 @@
 public class reversestring {
     public static void main(String[] args) {
 
-        String in = "java ja java jaaaa";
+
+//        comment and uncomment test cases
+        
+//      String in = "I Am Not String";
+//      String in = "JAVA JSP ANDROID";
+        String in = "1 22 333 4444 55555";
 
 
         //check the length of the intial string
@@ -13,7 +18,7 @@ public class reversestring {
         // have two pointers from each direction of array, i starting from 0 and j starting at L- 1
         // the points will move towards reach other until they overlap and loop will end
         for (int i = 0, j = l - 1; i <= j; i++, j--) {
-        //i and j will check if it contains a space and if it does, it will move to next position
+            //i and j will check if it contains a space and if it does, it will move to next position
             if (ans[i] == ' ') i++;
             if (ans[j] == ' ') j--;
 
